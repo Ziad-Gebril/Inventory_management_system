10b72d2a0567a3c87a870e898648a6d37bf25ad9
Farouk-Ezz-ElArab <faroukezz9595@gmail.com>
2025-10-16 21:32:55 +0300

    finished abstract database parent class

---
+++ database/Database.java
@@ -0,0 +1,67 @@
+package database;
+import java.io.*;
+import java.util.*;
+
+public abstract class Database<T> {
+    protected ArrayList<T> records = new ArrayList<>();
+    String fileName;
+
+    public Database(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public abstract T createRecordFrom(String line);
+
+    public ArrayList<T> returnAllRecords() {
+        return records;
+    }
+    public abstract String getSearchedKey(T record); // 3ashan a3rf a3ml search 3la ay 7aga f ay database
+
+    public void readFromFile() {
+        records.clear(); // 34an law nadet el function aktar mn marra ma y3mlsh duplicate
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String line = bufferedReader.readLine();
+            while (line != null) {
+                records.add(createRecordFrom(line));
+                line = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading from file: " + fileName);
+        }
+    }
+
+    public void insertRecord(T record) {
+        records.add(record);
+    }
+    public void deleteRecord(String key) {
+        records.removeIf(record -> getSearchedKey(record).equals(key));// dy badal ma a3ml loop 3la kol el records w a3ml compare 3la kol wa7da w a delete elly el key bta3ha yb2a equal lel key elly ana 3ayzo
+    }
+
+    public boolean contains(String key){
+        for(int i = 0; i < records.size(); i++){
+            if(getSearchedKey(records.get(i)).equals(key)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public T getRecord(String key){
+        for(int i = 0; i < records.size(); i++){
+            if(getSearchedKey(records.get(i)).equals(key)){
+                return records.get(i);
+            }
+        }
+        return null;
+    }
+    public void saveToFile() {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName))) {
+            for(int i = 0; i < records.size(); i++){
+                bufferedWriter.write(records.get(i).toString());
+                bufferedWriter.newLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Error saving to file: " + fileName);
+        }
+    }
+}
