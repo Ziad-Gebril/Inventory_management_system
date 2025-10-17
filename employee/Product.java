5a2ec8694b1de5844d2edeb67917c925b308669e
Islam Mohamed <hamadasemo0@gmail.com>
2025-10-17 16:05:42 +0300

    added variables

--- employee/Product.java
+++ employee/Product.java
@@ -1,5 +1,12 @@
 package employee;
 
 public class Product {
+    private String productID;
+    private String productName;
+    private String manufacturerName;
+    private String supplierName;
+    private int quantity;
+    private float price;
+
 
 }
