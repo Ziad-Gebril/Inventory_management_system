ffffc2813a911963ef6930a1723e3d858319de92
Islam Mohamed <hamadasemo0@gmail.com>
2025-10-17 16:06:11 +0300

    added constructor

--- employee/Product.java
+++ employee/Product.java
@@ -8,5 +8,13 @@ public class Product {
     private int quantity;
     private float price;
 
+    public Product(String productID, String productName, String manufacturerName, String supplierName, int quantity, float price) {
+        this.productID = productID;
+        this.productName = productName;
+        this.manufacturerName = manufacturerName;
+        this.supplierName = supplierName;
+        this.quantity = quantity;
+        this.price = price;
+    }
 
 }
