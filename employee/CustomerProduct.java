e3accabab0bb56166d72ca6932ab9ef103447920
Ziad Gebril <zgebro2005@gmail.com>
2025-10-17 16:32:41 +0300

    CustomerProduct done, (in addition to helping function date formating).

--- employee/CustomerProduct.java
+++ employee/CustomerProduct.java
@@ -1,15 +1,65 @@
 package employee;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class CustomerProduct {
 
     private String customerSSN;
     private String productID;
-    private LocalDate purchasDate;
+    private LocalDate purchaseDate;
     private Boolean paid;
-    
 
+    public CustomerProduct(String customerSSN , String productID, LocalDate purchaseDate)
+    {
+        this.customerSSN = customerSSN;
+        this.productID = productID;
+        this.purchaseDate = purchaseDate;
+        this.paid = false;
+    }
 
-    
+//******************************** getters ********************************************** */
+
+    public String getCustomerSSN()
+    {
+        return customerSSN;
+    }
+    public String getProductID()
+    {
+        return productID;
+    }
+    public LocalDate getPurchaseDate()
+    {
+        return purchaseDate;
+    }
+    public Boolean isPaid()
+    {
+        return paid;
+    }
+
+//********************************************* setters ***************************************** */
+
+    public void setPaid(Boolean paid)
+    {
+        this.paid = paid;
+    }
+
+    public String lineRepresentation()
+    {
+        String date = getDateFormated();
+        return customerSSN + "," + productID + "," + date + "," + paid;
+    }
+
+    public String getSearchKey()
+    {
+        return customerSSN;
+    }
+
+//************************************************** helper functions ******************************************* */
+
+    public String getDateFormated(){
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        String date = getPurchaseDate().format(formatter);
+        return date;
+    }
 }
