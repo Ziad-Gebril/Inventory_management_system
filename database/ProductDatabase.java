1f15c4890321531a2a484f808176f9242268e31d
Islam Mohamed <hamadasemo0@gmail.com>
2025-10-17 17:43:41 +0300

    created productsdatabase and extended from database override creatRecordFrom,getSearchKey,getLineRepresentation,ReturnAllRecords,getRecord

--- database/ProductDatabase.java
+++ database/ProductDatabase.java
@@ -2,7 +2,43 @@ package database;
 
 import employee.Product;
 
+import java.util.ArrayList;
+
 public class ProductDatabase extends Database<Product>{
 
+    public ProductDatabase(String fileName) {
+        super(fileName);
+    }
+
+    @Override
+    public Product createRecordFrom(String line) {
+        String[] parts = line.split(",");
+        return new Product(parts[0], parts[1], parts[2], parts[3], Integer.valueOf(parts[4]),Float.valueOf(parts[5]));
+    }
+
+    @Override
+    public String getSearchKey(Product product) {
+        return product.getSearchKey();
+    }
+
+    @Override
+    public String getLineRepresentation(Product product) {
+        return product.lineRepresentation();
+    }
+
+    @Override
+    public ArrayList<Product> returnAllRecords() {
+        return records;
+    }
+
+    @Override
+    public Product getRecord(String key){
+        for(int i = 0; i < records.size(); i++){
+            if(getSearchKey(records.get(i)).equals(key)){
+                return records.get(i);
+            }
+        }
+        return null;
+    }
 
 }
---
+++ employee/TestProduct.java
@@ -0,0 +1,27 @@
+package employee;
+
+import database.ProductDatabase;
+
+public class TestProduct {
+    public static void main(String[] args) {
+
+        ProductDatabase db = new ProductDatabase("database/Product.txt");
+        db.readFromFile();
+
+        System.out.println("Current products loaded from file:");
+        for (Product e : db.returnAllRecords()) {
+            System.out.println(e.lineRepresentation());
+        }
+
+        Product newProd =new Product("P5","EarBuds","Huawei","TechSupplier",5,500);
+        db.insertRecord(newProd);
+
+        db.saveToFile();
+
+        System.out.println("\nAfter adding EarBuds and saving:");
+        for (Product e : db.returnAllRecords()) {
+            System.out.println(e.lineRepresentation());
+        }
+    }
+
+}
