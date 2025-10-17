f56fda26365ba528420eeaea2153598273f01af6
Islam Mohamed <hamadasemo0@gmail.com>
2025-10-17 18:08:29 +0300

    Finished testing all methods in ProductDatabase

--- employee/TestProduct.java
+++ employee/TestProduct.java
@@ -2,11 +2,25 @@ package employee;
 
 import database.ProductDatabase;
 
+import java.util.ArrayList;
+
 public class TestProduct {
     public static void main(String[] args) {
 
+        ArrayList<Product> products;
+
         ProductDatabase db = new ProductDatabase("database/Product.txt");
         db.readFromFile();
+
+        products=db.returnAllRecords();
+        System.out.println(products);
+
+        db.insertRecord(db.createRecordFrom("P7,Tablet,Samsung,TechSupplier,5,1000"));
+        db.saveToFile();
+
+        System.out.println(db.getRecord("P1"));
+
+        db.returnAllRecords();
 
         System.out.println("Current products loaded from file:");
         for (Product e : db.returnAllRecords()) {
@@ -22,8 +36,11 @@ public class TestProduct {
         for (Product e : db.returnAllRecords()) {
             System.out.println(e.lineRepresentation());
         }
+
         System.out.println(db.contains("P6"));
+
         db.deleteRecord("P5");
+
         db.saveToFile();
     }
 
