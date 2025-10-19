a58eb10b46c590d700273cb403f66cf94e5adebb
Farouk Ezz <faroukezz9595@example.com>
2025-10-19 20:00:04 +0300

    added template for menu

--- MainMenu.java
+++ MainMenu.java
@@ -1,8 +1,8 @@
-
-
 import java.time.LocalDate;
 import java.time.format.*;
-
+import java.util.*;
+import utils.Generator;
+import utils.Validator;
 public class MainMenu {
 
     final String RESET = "\u001B[0m";
@@ -14,10 +14,24 @@ public class MainMenu {
     final String CYAN = "\u001B[36m";
     final String WHITE = "\u001B[37m";
     
+    int choice;
+    Scanner scanner = new Scanner(System.in);
+    public void showMainMenu() {
+        System.out.println(BLUE + "================= Inventory Management System =================" + RESET);
+        System.out.println(GREEN + "1. Manage Products" + RESET);
+        System.out.println(GREEN + "2. Manage Suppliers" + RESET);
+        System.out.println(GREEN + "3. Manage Orders" + RESET);
+        System.out.println(GREEN + "4. View Reports" + RESET);
+        System.out.println(GREEN + "5. Exit" + RESET);
+        System.out.println(BLUE + "==============================================================" + RESET);
+        System.out.print(YELLOW + "Please select an option (1-5): " + RESET);
+    }
 
 
 
-    //**************************************Helping Functions******************
+
+
+    //**************************************Helping Functions***********************************//
 
     public String getDateFormated(LocalDate Date) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
