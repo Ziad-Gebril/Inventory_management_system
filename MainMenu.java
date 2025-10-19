f2da78cc742cf283274030b8fa5d09ef4cbad497
Ziad Gebril <zgebro2005@gmail.com>
2025-10-19 20:42:52 +0300

    started work at main menu

--- MainMenu.java
+++ MainMenu.java
@@ -3,6 +3,11 @@ import java.time.format.*;
 import java.util.*;
 import utils.Generator;
 import utils.Validator;
+import java.lang.InterruptedException;
+import java.io.IOException;
+import database.*;
+import customer_product.*;
+
 public class MainMenu {
 
     final String RESET = "\u001B[0m";
@@ -14,18 +19,39 @@ public class MainMenu {
     final String CYAN = "\u001B[36m";
     final String WHITE = "\u001B[37m";
     
-    int choice;
-    Scanner scanner = new Scanner(System.in);
-    public void showMainMenu() {
+   
+    
+    public void LoginMenu() {
         System.out.println(BLUE + "================= Inventory Management System =================" + RESET);
-        System.out.println(GREEN + "1. Manage Products" + RESET);
-        System.out.println(GREEN + "2. Manage Suppliers" + RESET);
-        System.out.println(GREEN + "3. Manage Orders" + RESET);
-        System.out.println(GREEN + "4. View Reports" + RESET);
-        System.out.println(GREEN + "5. Exit" + RESET);
+        System.out.println(GREEN + "1. Admin Login" + RESET);
+        System.out.println(GREEN + "2. Employee Login" + RESET);
+        System.out.println(GREEN + "3. Exit" + RESET);
         System.out.println(BLUE + "==============================================================" + RESET);
-        System.out.print(YELLOW + "Please select an option (1-5): " + RESET);
+        System.out.print(YELLOW + "Please select an option (1 --> 3): " + RESET);
+
+         
+    Scanner scan = new Scanner(System.in);
+    int choice = scan.nextInt();
+
+        switch(choice)
+        {
+            case 1:
+            System.out.println("under construction...");            
+
+            case 2:
+            System.out.println("under construction...");
+
+            case 3:
+            System.out.println("under construction...");
+            System.exit(0);
+
+            default :
+            System.out.println(RED + "Invalid Choice! Please try again." + RESET);
+            LoginMenu();
+        }
     }
+
+    
 
 
 
@@ -43,6 +69,19 @@ public class MainMenu {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
         LocalDate Date = LocalDate.parse(date,formatter);
         return Date;
+    }
+
+    static void clearScreen() {
+        try {
+            if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                new ProcessBuilder("clear").inheritIO().start().waitFor();
+            }
+        } catch (IOException | InterruptedException ex) {
+            ex.printStackTrace();
+        }
+
     }
 
 }
